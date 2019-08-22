80f91c52209c6e2c5fc3b203815c92e10da48438
ohaionm <moshe@doit-intl.com>
2019-08-22 14:55:09 +0300

    mosheo: add email alert

--- alert_channels.py
+++ alert_channels.py
@@ -1,15 +1,15 @@
 import json
 
 import requests
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
-def send_slack_alert(wekbook_url, job, fields_to_retrieve, cost):
-    details = ""
-    for field in fields_to_retrieve:
-        details = details + field + "=" + str(getattr(job, field, "Non")) + ", "
+def send_slack_alert(wekbook_url, details):
+
 
     data = {
-        'text': 'Job crossed threshold. Job details: ' + details + 'cost=' + str(cost),
+        'text': 'Job crossed threshold. Job details: ' + details,
         'username': 'moshe'
     }
 
@@ -17,3 +17,18 @@ def send_slack_alert(wekbook_url, job, fields_to_retrieve, cost):
         data), headers={'Content-Type': 'application/json'})
 
     return str(response.status_code)
+
+
+def send_email_alert(sender, recipients, details):
+
+    email_body = "Hey, <br> Job crossed the cost limit. Following is the job details:<br><strong>" + details + "</strong>"
+    message = Mail(
+        from_email=sender,
+        to_emails=recipients,
+        subject='BigQuery job crossed threshold',
+        html_content='<strong>' + email_body + '</strong>')
+    try:
+        sg = SendGridAPIClient('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')
+        sg.send(message)
+    except Exception as e:
+        print(e.message)
--- main.py
+++ main.py
@@ -10,12 +10,8 @@ config = json.loads(data)
 
 
 def bq_informer(data, context):
-    print(context.resource.get('name'))
     alert_threshold = config['ALERT_THRESHOLD']
     tera_bytes_cost = config['TB_COST']
-    wekbook_url = config['SLACK_WEBHOOK_URL']
-    slack_alert = config['SLACK_ALERT']
-    fields_to_retrieve = config['FIELDS_TO_RETRIEVE']
     client = bigquery.Client()
     resource_name = context.resource.get('name')
     slicing_index = resource_name.rfind('/') + 1
@@ -24,7 +20,25 @@ def bq_informer(data, context):
     total_tera_bytes_billed = job.total_bytes_billed / 1000000000000
     total_cost = total_tera_bytes_billed / tera_bytes_cost
     if total_cost >= alert_threshold:
+        print("Job violated cost threshold limit")
+        fields_to_retrieve = config['FIELDS_TO_RETRIEVE']
+        details = ""
+        for field in fields_to_retrieve:
+            details = details + field + "=" + str(getattr(job, field, "Non")) + ", "
+        details = details + 'cost=' + str(total_cost)
+
+        print("Job details: \n" + details)
+        slack_alert = config['SLACK_ALERT']
         if slack_alert:
-            alert_channels.send_slack_alert(wekbook_url, job, fields_to_retrieve, total_cost)
+            print("Sending slack alert")
+            wekbook_url = config['SLACK_WEBHOOK_URL']
+            alert_channels.send_slack_alert(wekbook_url, details)
+
+        email_alert = config['EMAIL_ALERT']
+        if email_alert:
+            print("Sending email alert")
+            sender = config['EMAIL_SENDER']
+            recipients = config['EMAIL_RECIPIENTS']
+            alert_channels.send_email_alert(sender, recipients, job, details)
     else:
-        print("alert won't be sent")
+        print("Job didn't violate cost threshold limit")
